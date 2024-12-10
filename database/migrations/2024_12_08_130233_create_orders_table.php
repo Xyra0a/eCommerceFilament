279cb82915044379455b2c09856bd79750df9f82
xyrakyro <faridappratama@gmail.com>
2024-12-10 22:00:22 +0700

    buat category, users filament

--- database/migrations/2024_12_08_130233_create_orders_table.php
+++ database/migrations/2024_12_08_130233_create_orders_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->enum('status', ['new', 'processing', 'shipped', 'delivered', 'canceled'])->default('new');
             $table->string('currency')->nullable();
             $table->decimal('shipping_amount', 10, 2)->nullable();
-            $table->string('payment_method')->nullable();
+            $table->string('shipping_method')->nullable();
             $table->text('notes')->nullable();
             $table->timestamps();
         });
